902788aea2c7138c64174f6e624316a640a95369
Tony Hagwall <hagwall86@live.se>
2023-11-08 10:52:37 +0100

    fix getDocSchema typing

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -3,7 +3,7 @@ import {z} from 'zod';
 export const getDocumentSchema = z.object({
     id: z.number(),
     title: z.string().min(1, 'Title is required.').max(255),
-    description: z.string().min(1, 'Description is required.'),
+    content: z.string().min(1, 'Content is required.'),
 });
 
 export const createDocumentSchema = z.object({
