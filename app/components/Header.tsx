7f5377e8ab16d9c810696af73e5ff507d5498be1
Therese Hagwall <theresejager@live.se>
2023-11-13 11:20:53 +0100

    Add new color

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -43,9 +43,9 @@ export default function Header() {
                         <Link
                             key={link.href}
                             className={classnames({
-                                "text-stone-500": link.href === currentPath,
-                                "text-stone-700": link.href !== currentPath,
-                                "hover:text-stone-800 transition-colors": true,
+                                "text-primary": link.href === currentPath,
+                                "text-secondary": link.href !== currentPath,
+                                "hover:text-primary transition-colors": true,
                             })}
                             href={link.href}
                         >
